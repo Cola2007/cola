c298463512bcd936bbffc92dbe336a2426638773
Moxie Devoloper Team <121292566+nipuna15@users.noreply.github.com>
2023-06-28 06:23:23 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -59,7 +59,7 @@ async function ig(linak){
   const scrape = await axios.get(`https://www.hirunews.lk/local-news.php?pageID=1`)
   const $g = cheerio.load(scrape.data)
   const link = $g('/html/body/div[9]/div[1]/div[1]/div/div[2]/div[2]/div[1]/a[1]').attr('href')  
-  const img = $g('/html/body/div[9]/div[1]/div[1]/div/div[2]/div[1]/div/a/img).attr('src') 
+  const img = $g('/html/body/div[9]/div[1]/div[1]/div/div[2]/div[1]/div/a/img').attr('src') 
   const scrape2 = await axios.get(link)
   const $i = cheerio.load(scrape2.data)
   const desc2 = $i('//*[@id="article-phara2"]').text() 
