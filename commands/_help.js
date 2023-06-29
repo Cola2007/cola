788a7b219fdbfa5c962e4b5a2cd88674d44eeb15
Moxie Devoloper Team <121292566+nipuna15@users.noreply.github.com>
2023-06-29 14:58:05 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -116,7 +116,7 @@ Secktor.cmd({
 for (let i = 0; i < commands.length; i++) 
 {
      if(commands[i].pattern==undefined) continue
-     str += `📗 ${fancytext(commands[i].pattern,4)}\n` 
+     str += `📗 ◉ ${fancytext(commands[i].pattern,1)}\n` 
      str += `🍁 ${fancytext(commands[i].desc,1)}\n\n`
 }
             return Void.sendMessage(citel.chat, { image: { url: THUMB_IMAGE }, caption: str })
