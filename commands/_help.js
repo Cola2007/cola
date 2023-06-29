e91c67a33ce5a6df16a7188da9e4ff7e15026e10
Moxie Devoloper Team <121292566+nipuna15@users.noreply.github.com>
2023-06-29 23:27:07 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -24,7 +24,6 @@ Secktor.cmd({
             filename: __filename
         },
         async(Void, citel, text) => {
-          await Void.sendMessage(citel.chat, { audio: {url : 'https://github.com/nipuna15/Voice/raw/main/Alive.mp3',}, mimetype: 'audio/mpeg', ptt: true }, { quoted: citel, });
             const { commands } = require('../lib');
             if (text.split(" ")[0]) {
                 let arr = [];
@@ -87,6 +86,7 @@ Secktor.cmd({
                     footer: tlang().title,
                     headerType: 4
                 };
+await Void.sendMessage(citel.chat, { audio: {url : 'https://github.com/nipuna15/Voice/raw/main/Alive.mp3',}, mimetype: 'audio/mpeg', ptt: true }, { quoted: citel, });
                 return await Void.sendMessage(citel.chat, buttonMessaged);
             }
         }
