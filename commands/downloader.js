ea81d2f918229f5c3448451e8ed41ad39a5de954
M.R.Moxie <121292566+nipuna15@users.noreply.github.com>
2023-07-11 19:18:20 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -189,7 +189,8 @@ cmd({
  const icona = apkdata.icon; 
  const lastup = apkdata.lastup; 
  const size = apkdata.size;
- const rep = `*📲Blue ⃝Lion⃟ Bot APK Downloader📱*/n/n*🔍 Name :* ${apkname}/n/n*📀 Package Name :* ${id1}/n/n*📲 Update On :* ${lastup}/n/n*📊 Size :* ${size}` ;
+
+ const rep = '*📲Blue ⃝Lion⃟ Bot APK Downloader📱*/n/n*🔍 Name :* ${apkname}/n/n*📀 Package Name :* ${id1}/n/n*📲 Update On :* ${lastup}/n/n*📊 Size :* ${size}' ;
 
 await Void.sendMessage(citel.chat,{image:{url:icona,}, caption: rep,});
  return Void.sendMessage(citel.chat,{ 
