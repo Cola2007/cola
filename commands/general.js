f9591b9463aec0e0f65d722361ecac35ee11faa5
Nipuna <121292566+nipuna15@users.noreply.github.com>
2023-07-24 22:29:13 +0530

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -13,6 +13,7 @@ const { tlang, botpic, cmd, prefix, runtime, Config , sleep } = require('../lib'
 const axios = require('axios')
 const speed = require('performance-now')
 const fetch = require('node-fetch');
+const { Configuration, OpenAIApi } = require("openai");
 //---------------------------------------------------------------------------
 cmd({
     pattern: "chat",
@@ -23,43 +24,17 @@ cmd({
     filename: __filename,
 },
 async(Void, citel,text) => {
-    let zx = text.length;
-    if (zx < 8) {
-        let {data} = await axios.get(`http://api.brainshop.ai/get?bid=167991&key=aozpOoNOy3dfLgmB&uid=[${citel.sender.split("@")[0]}]&msg=[${text}]`);
-        return citel.reply(data.cnt);  
-    }
-    if (!text) return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
-     // const { Configuration, OpenAIApi } = require("openai");
-    // const configuration = new Configuration({
-    //     apiKey: Config.OPENAI_API_KEY || "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",
-    // });
-    // const openai = new OpenAIApi(configuration);
-    // const completion = await openai.createCompletion({
-    //     model: "text-davinci-002",
-    //     prompt: text,
-    //     temperature: 0.5,
-    //     max_tokens: 80,
-    //     top_p: 1.0,
-    //     frequency_penalty: 0.5,
-    //     presence_penalty: 0.0,
-    //     stop: ['"""'],
-    // });
-    // citel.reply(completion.data.choices[0].text);
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${Config.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo", 
-      messages: [{ role: "system", content: "You" }, { role: "user", content: text }],
-    }),
-  });
+  const mg = text;
+const configuration = new Configuration({
+  apiKey: Config.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-  const data = await response.json();
-  console.log("GPT REPONCE : ",data); 
-  if (!data.choices || data.choices.length === 0) {citel.reply("*Invalid ChatGPT API Key, Please Put New Key*"); }
+const completion = await openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{"role": "system", "content": `${mg}`}, {role: "user", content: "Hello world"}],
+});
+
   return await  citel.reply(data.choices[0].message.content)
 }
 )
