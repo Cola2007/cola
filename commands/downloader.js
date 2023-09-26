ee09fdc08e98706acec20842ffc9e6100d02a266
Cola2007 <144465284+Cola2007@users.noreply.github.com>
2023-09-26 18:04:55 +0530

    b

--- commands/downloader.js
+++ commands/downloader.js
@@ -109,7 +109,7 @@ let video = await axios.post(domain+'api/', {}, {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
         },
         params: {
-            url: url,
+            url: text,
             count: 12,
             cursor: 0,
             web: 1,
