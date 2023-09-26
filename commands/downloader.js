c71694c5bbd0443e743ad9d9820f6840759e3ad4
Cola2007 <144465284+Cola2007@users.noreply.github.com>
2023-09-26 17:54:51 +0530

    v

--- commands/downloader.js
+++ commands/downloader.js
@@ -99,8 +99,25 @@ cmd({
 
 async(Void, citel, text) => { 
 if(!text) return await citel.reply(`*Uhh Please, Provide me tiktok Video Url*\n*_Ex .tiktok https://www.tiktok.com/@dakwahmuezza/video/7150544062221749531_*`); 
-	let video = await tiktokDL(text);
-    return await Void.sendMessage(citel.chat, {video : {url : video.nowm  } , caption: "POWERD BY BLUE-LION" } , {quoted : citel });
+const domain = 'https://www.tikwm.com/';
+let video = await axios.post(domain+'api/', {}, {
+        headers: {
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            //'cookie': 'current_language=en; _ga=GA1.1.512520618.1695715681; _gcl_au=1.1.1831499197.1695715682; _ga_5370HT04Z3=GS1.1.1695718051.2.0.1695718051.0.0.0',
+            'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+        },
+        params: {
+            url: url,
+            count: 12,
+            cursor: 0,
+            web: 1,
+            hd: 1
+        }
+    })
+
+    return await Void.sendMessage(citel.chat, {video : {url : domain+video.data.hdplay } , caption: "POWERD BY BLUE-LION" } , {quoted : citel });
 }) 
      //--------------------------------------------------------------------------- 
  cmd({ 
