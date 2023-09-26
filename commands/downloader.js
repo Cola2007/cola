842001778db052af4b50ab012a3962bb6b59308a
Cola2007 <144465284+Cola2007@users.noreply.github.com>
2023-09-26 13:44:06 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -1,6 +1,6 @@
 const { tlang, ringtone, cmd,fetchJson, sleep, botpic,ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib') 
  const { mediafire } = require("../lib/mediafire.js"); 
- const {Tiktok} = require("../lib/ttdl.js"); 
+ const {TiktokDL} = require("../lib/ttdl.js"); 
  const cheerio = require('cheerio'); 
  const fbInfoVideo = require('fb-info-video'); 
  const request = require('request'); 
@@ -99,8 +99,8 @@ cmd({
 
 async(Void, citel, text) => { 
 if(!text) return await citel.reply(`*Uhh Please, Provide me tiktok Video Url*\n*_Ex .tiktok https://www.tiktok.com/@dakwahmuezza/video/7150544062221749531_*`); 
-	let video = await Tiktok(text);
-    return await Void.sendMessage(citel.chat, {video : {url : video.result.nowm  } , caption: "POWERD BY BLUE-LION" } , {quoted : citel });
+	let video = await TiktokDL(text);
+    return await Void.sendMessage(citel.chat, {video : {url : video.nowm  } , caption: "POWERD BY BLUE-LION" } , {quoted : citel });
 }) 
      //--------------------------------------------------------------------------- 
  cmd({ 
